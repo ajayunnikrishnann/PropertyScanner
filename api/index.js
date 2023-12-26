f5822d86deb538dae9fff5935d74fdb40ce010f8
ajaykrishnan <ajayunnikrishnan97@gmail.com>
2023-12-26 11:21:31 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
- 
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log('Connected to Mongodb');
+}).catch((err)=>{
+    console.log(err);
+})
+
 const app = express();``
 
 app.listen(3000,()=>{
