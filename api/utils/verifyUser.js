7ed6a33dd1d5889f53d9b5c91b1f2deb40054b57
ajaykrishnan <ajayunnikrishnan97@gmail.com>
2024-01-29 10:03:19 +0530

    create get user listings api route

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -7,6 +7,7 @@ export const verifyToken = (req,res,next) =>{
 
     if(!token) return next(errorHandler(401,'Unauthorized'));
 
+    
     jwt.verify(token,process.env.JWT_CODE, (err,user) => {
         if(err) return next (errorHandler(403,'Forbidden'));
 
